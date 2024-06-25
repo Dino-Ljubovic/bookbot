7fc2f0c1d04f556f1b2a61793fa7043c02d685d5
Dino-Ljubovic <dinol93@gmail.com>
2024-06-25 16:24:03 -0700

    commented code for readability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import string
-
+# Grabbing frankenstein book text
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
@@ -12,16 +12,16 @@ def main():
     
     char_list = char_count(file_contents)  # Get the character list
     char_list.sort(reverse=True, key=sort_on)  # Sort the list
-
+    # Printing report for character count
     print("--- Begin report of books/frankenstein.txt ---")
     for item in char_list:
         print(f"The '{item['char']}' character was found {item['num']} times")
     print("--- End report ---")
-    
+# Splitting file contents text into word list
 def word_count(file_contents):
     word_list = file_contents.split()
     return len(word_list)
-    
+# creating dictionary with char -> count key value pair     
 def char_count(file_contents):
     allowed_characters = string.ascii_letters
     char_counts = {}
@@ -31,9 +31,10 @@ def char_count(file_contents):
                 char_counts[letter] = 1
             else:
                 char_counts[letter] += 1
+    # Splitting char_count dictionary into list of key value pairs for sorting
     char_list = [{"char": k, "num": v} for k, v in char_counts.items()]
     return char_list
-
+# Sorting char_list
 def sort_on(item):
     return item["num"]
 
