514aa020b522818f6337e4ff9b30dc47233f5380
Dino-Ljubovic <dinol93@gmail.com>
2024-06-25 15:31:52 -0700

    complete main.py, adding sorted character count breakdown

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import string
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    
+    # Printing a small portion of the book for readability
+    preview_length = min(len(file_contents), 200)  # Print first 200 characters or less if file is shorter
+    print(file_contents[:preview_length] + '...')  # Print first 200 chars with ellipsis if longer
+    
     print(f"This book contains {word_count(file_contents)} words!")
-    print(char_count(file_contents))   
+    
+    char_list = char_count(file_contents)  # Get the character list
+    char_list.sort(reverse=True, key=sort_on)  # Sort the list
 
+    print("--- Begin report of books/frankenstein.txt ---")
+    for item in char_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
+    
 def word_count(file_contents):
     word_list = file_contents.split()
     return len(word_list)
@@ -20,11 +31,11 @@ def char_count(file_contents):
                 char_counts[letter] = 1
             else:
                 char_counts[letter] += 1
-    return char_counts
+    char_list = [{"char": k, "num": v} for k, v in char_counts.items()]
+    return char_list
 
-def sort_on(char_counts):
-    return char_counts["num"]
+def sort_on(item):
+    return item["num"]
 
-char_counts.sort(reverse=True, key=sort_on)
 main()
 
